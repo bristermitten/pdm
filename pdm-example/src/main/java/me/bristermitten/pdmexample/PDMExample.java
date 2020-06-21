1a3a91aa011a43cecad260702c6b841748bb13bf
Alexander Wood <alexljwood24@hotmail.co.uk>
2020-06-21 21:36:54 +0100

    Add example module

---
+++ pdm-example/src/main/java/me/bristermitten/pdmexample/PDMExample.java
@@ -0,0 +1,17 @@
+package me.bristermitten.pdmexample;
+
+import me.bristermitten.pdm.PluginDependencyManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PDMExample extends JavaPlugin
+{
+
+    @Override
+    public void onEnable()
+    {
+        PluginDependencyManager dependencyManager = new PluginDependencyManager(this);
+        dependencyManager.loadAllDependencies().thenRun(() -> {
+            System.out.println("All loaded!");
+        });
+    }
+}
