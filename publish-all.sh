f1284b62bea1e98a6cd16a53937ffb16ab0554f4
Alexander Wood <alexljwood24@hotmail.co.uk>
2020-07-24 00:50:28 +0100

    Add publish-all.sh, that runs the Gradle publish task for all modules

---
+++ publish-all.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+echo Publishing Common Lib...
+gradle common-lib:publish
+echo Done!
+
+if $? != 0; then
+  exit $?
+fi
+
+echo Publishing PDM Runtime...
+gradle pdm:publish
+echo Done!
+if $? != 0; then
+  exit $?
+fi
+
+echo Publishing Gradle Plugin...
+gradle pdm-gradle:publish
+echo Done!
