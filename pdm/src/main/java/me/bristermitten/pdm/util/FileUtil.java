05d97b59b276247231f470b0a4616d6e6ddcfbca
Alexander Wood <alexljwood24@hotmail.co.uk>
2020-07-24 02:09:49 +0100

    Update FileUtil to use Channel based writing when available

--- pdm/src/main/java/me/bristermitten/pdm/util/FileUtil.java
+++ pdm/src/main/java/me/bristermitten/pdm/util/FileUtil.java
@@ -3,6 +3,11 @@ package me.bristermitten.pdm.util;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 
 public final class FileUtil
 {
@@ -13,6 +18,7 @@ public final class FileUtil
 
     /**
      * Create a given directory if it does not exist.
+     *
      * @param file the directory to create.
      */
     public static void createDirectoryIfNotPresent(@NotNull final File file)
@@ -21,6 +27,15 @@ public final class FileUtil
         {
             //noinspection ResultOfMethodCallIgnored
             file.mkdirs();
+        }
+    }
+
+    public static void writeFrom(@NotNull final File file, @NotNull final InputStream inputStream) throws IOException
+    {
+        try (final ReadableByteChannel channel = Channels.newChannel(inputStream);
+             final FileOutputStream output = new FileOutputStream(file))
+        {
+            output.getChannel().transferFrom(channel, 0, Long.MAX_VALUE);
         }
     }
 }
