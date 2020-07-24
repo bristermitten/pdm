67ac599ab9fde03cd77dfcee5d4ec4304181516d
Alexander Wood <alexljwood24@hotmail.co.uk>
2020-07-24 02:08:23 +0100

    Update the example module to a) actually work b) not have everything commented out

--- example/src/main/java/me/bristermitten/pdmexample/PDMExample.java
+++ example/src/main/java/me/bristermitten/pdmexample/PDMExample.java
@@ -1,15 +1,16 @@
 package me.bristermitten.pdmexample;
 
-//import me.bristermitten.pdm.PluginDependencyManager;
+import me.bristermitten.pdm.PluginDependencyManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PDMExample extends JavaPlugin
 {
-//
-//    @Override
-//    public void onEnable()
-//    {
-//        PluginDependencyManager dependencyManager = new PluginDependencyManager(this);
-//        dependencyManager.loadAllDependencies().thenRun(() -> getLogger().info("All Loaded!"));
-//    }
+
+    @Override
+    public void onEnable()
+    {
+        PluginDependencyManager dependencyManager = new PluginDependencyManager(this);
+        dependencyManager.loadAllDependencies().
+                thenRun(() -> getLogger().info("All Loaded Asynchronously!"));
+    }
 }
