9e399fe4a088b311aaaeb2cbcf25c5ff45b204c3
Alexander Wood <alexljwood24@hotmail.co.uk>
2020-08-07 17:26:55 +0100

    Update publish-all.sh

--- publish-all.sh
+++ publish-all.sh
@@ -3,16 +3,9 @@ echo Publishing Common Lib...
 ./gradlew common-lib:publish
 echo Done!
 
-if $? != 0; then
-  exit $?
-fi
-
 echo Publishing PDM Runtime...
 ./gradlew pdm:publish
 echo Done!
-if $? != 0; then
-  exit $?
-fi
 
 echo Publishing Gradle Plugin...
 ./gradlew pdm-gradle:publishPlugins
