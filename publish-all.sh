01ddf8f51e68a1a438a6dd68cb3ff2881f3b66dc
Alex Wood <alexljwood24@hotmail.co.uk>
2020-08-15 17:57:00 +0100

    Potential CI workaround

--- publish-all.sh
+++ publish-all.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
 echo "gradle.publish.key=$APIKEY" >> gradle.properties
 echo "gradle.publish.secret=$APISECRET" >> gradle.properties
-#echo Publishing Common Lib...
-#./gradlew common-lib:publish
-#echo Done!
 
-echo Publishing PDM Runtime...
-./gradlew pdm:publish
-echo Done!
+./gradlew publishAllToMaven
 
-echo Publishing Gradle Plugin...
 ./gradlew pdm-gradle:publishPlugins
-echo Done!
