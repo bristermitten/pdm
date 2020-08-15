ae3e804aaee01759b4300d22e2fa17661b1c5aa0
Alex Wood <alexljwood24@hotmail.co.uk>
2020-08-15 17:23:44 +0100

    Another test commit

--- publish-all.sh
+++ publish-all.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 echo "gradle.publish.key=$APIKEY" >> gradle.properties
 echo "gradle.publish.secret=$APISECRET" >> gradle.properties
-echo Publishing Common Lib...
-./gradlew common-lib:publish
-echo Done!
+#echo Publishing Common Lib...
+#./gradlew common-lib:publish
+#echo Done!
 
 echo Publishing PDM Runtime...
 ./gradlew pdm:publish
