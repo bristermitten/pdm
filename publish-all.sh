567db7f3c54c8ceadc48b6b1b38501fc2e9244ff
Alexander Wood <alexwood2403@gmail.com>
2020-10-26 11:34:21 +0000

    Publishing now does a clean build, republishing 0.0.29

--- publish-all.sh
+++ publish-all.sh
@@ -2,6 +2,6 @@
 echo "gradle.publish.key=$APIKEY" >> gradle.properties
 echo "gradle.publish.secret=$APISECRET" >> gradle.properties
 
-./gradlew publishAllToMaven
+./gradlew clean publishAllToMaven
 
-./gradlew pdm-gradle:publishPlugins
+./gradlew clean pdm-gradle:publishPlugins
