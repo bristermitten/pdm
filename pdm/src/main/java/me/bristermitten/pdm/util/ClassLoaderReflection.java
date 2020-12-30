80c0fe206c14fe59537ca5fddabd23beaa21c58a
jaimss <james@harrell365.com>
2020-12-29 20:24:51 -0500

    fix java9+ reflection warning (somewhat tested)

--- pdm/src/main/java/me/bristermitten/pdm/util/ClassLoaderReflection.java
+++ pdm/src/main/java/me/bristermitten/pdm/util/ClassLoaderReflection.java
@@ -16,6 +16,17 @@ public class ClassLoaderReflection
     {
         final Method addURL;
 
+        // open the classloader module for java9+ so it wont have a warning
+        try
+        {
+            openUrlClassLoaderModule();
+        }
+        catch (Throwable ignored)
+        {
+            // ignore exception. Java 8 wont have the module, so it wont matter if we ignore it
+            // cause there will be no warning
+        }
+
         try
         {
             addURL = URLClassLoader.class.getDeclaredMethod("addURL", URL.class);
@@ -44,5 +55,20 @@ public class ClassLoaderReflection
         {
             throw new IllegalArgumentException(exception);
         }
+    }
+
+    @SuppressWarnings("JavaReflectionInvocation")
+    private static void openUrlClassLoaderModule() throws Exception
+    {
+        // Taken from LuckPerms
+
+        Class<?> moduleClass = Class.forName("java.lang.Module");
+        Method getModuleMethod = Class.class.getMethod("getModule");
+        Method addOpensMethod = moduleClass.getMethod("addOpens", String.class, moduleClass);
+
+        Object urlClassLoaderModule = getModuleMethod.invoke(URLClassLoader.class);
+        Object thisModule = getModuleMethod.invoke(ClassLoaderReflection.class);
+
+        addOpensMethod.invoke(urlClassLoaderModule, URLClassLoader.class.getPackage().getName(), thisModule);
     }
 }
