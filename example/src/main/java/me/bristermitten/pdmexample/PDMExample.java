e154cd9a66d65cff53c93faa2f188be26ee2267a
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-01-31 21:24:33 +0000

    Modernize the example module: - Update Kotlin version - Add comprehensive documentation to both examples - Target the latest PDM api

--- example/src/main/java/me/bristermitten/pdmexample/PDMExample.java
+++ example/src/main/java/me/bristermitten/pdmexample/PDMExample.java
@@ -1,17 +1,23 @@
 package me.bristermitten.pdmexample;
 
-import me.bristermitten.pdm.PDMBuilder;
 import me.bristermitten.pdm.PluginDependencyManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PDMExample extends JavaPlugin
 {
-
     @Override
     public void onEnable()
     {
+        //Create a new PluginDependencyManager with data from the JavaPlugin
         PluginDependencyManager dependencyManager = PluginDependencyManager.of(this);
-        dependencyManager.loadAllDependencies().thenRun(
-                () -> getLogger().info("All Loaded Asynchronously!"));
+
+        dependencyManager
+                .loadAllDependencies() //Reads dependencies.json and starts downloading / loading all dependencies async
+                .thenRun(() -> getLogger().info("All Loaded Asynchronously!")); //The lambda will run once everything is loaded
+
+        /*
+        Note that you can also force the server to wait for the dependencies to be downloaded with join(),
+        but this is not advised as it can cause lag. See PDMExampleKotlin.kt for an example of this
+         */
     }
 }
--- example/src/main/kotlin/me/bristermitten/pdmexample/PDMExampleKotlin.kt
+++ example/src/main/kotlin/me/bristermitten/pdmexample/PDMExampleKotlin.kt
@@ -1,16 +1,21 @@
 package me.bristermitten.pdmexample
 
-import me.bristermitten.pdm.PDMBuilder
 import me.bristermitten.pdm.PluginDependencyManager
 import org.bukkit.plugin.java.JavaPlugin
 
-
 class PDMExampleKotlin : JavaPlugin()
 {
-
 	override fun onEnable()
 	{
-		val dependencyManager = PDMBuilder(this).build() ?: return //Necessary to bypass Intrinsics generation
-		dependencyManager.loadAllDependencies().join()
+		/*
+		Note that these methods must be chained in Kotlin to avoid Intrinsics generation, which will cause
+		ClassNotFoundExceptions if the Kotlin stdlib is not present on the classpath.
+
+		Alternatively, make a Java bootstrap class that calls PDM, or use any of Kotlin's null safety operators
+		which should also avoid this issue
+		 */
+		PluginDependencyManager.of(this) //Create a new PluginDependencyManager with data from the JavaPlugin
+			.loadAllDependencies() //Reads dependencies.json and starts downloading / loading all dependencies async
+			.join() //Block until all dependencies are downloaded - in production a callback is advised to avoid lag
 	}
 }
