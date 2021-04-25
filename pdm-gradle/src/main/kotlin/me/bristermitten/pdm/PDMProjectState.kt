28c7f1a1aa6c67cc0bb55a7831ed5cc26c83ecc1
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-04-25 19:35:37 +0100

    Remove all the old crap

--- pdm-gradle/src/main/kotlin/me/bristermitten/pdm/PDMProjectState.kt
+++
@@ -1,9 +0,0 @@
-package me.bristermitten.pdm
-
-import java.io.File
-
-data class PDMProjectState(
-		val cacheFile: File
-)
-{
-}
--- publish-all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo "gradle.publish.key=$APIKEY" >> gradle.properties
-echo "gradle.publish.secret=$APISECRET" >> gradle.properties
-
-./gradlew clean publishAllToMaven
-
-./gradlew clean pdm-gradle:publishPlugins
