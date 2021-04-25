e7618d1ace7682a0a0a9066687db25b511fe761b
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-04-25 20:40:51 +0100

    Project type

--- repo-scraper/src/main/java/me/bristermitten/pdm/reposcraper/entity/Project.java
+++ repo-scraper/src/main/java/me/bristermitten/pdm/reposcraper/entity/Project.java
@@ -4,5 +4,9 @@ package me.bristermitten.pdm.reposcraper.entity;
  * An artifact in a Maven Repository
  */
 public class Project {
+    private final String groupId;
 
+    public Project(String groupId) {
+        this.groupId = groupId;
+    }
 }
